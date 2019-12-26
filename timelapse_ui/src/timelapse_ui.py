7d619d0f8c067d912e230fda472d3ae97a86af15
Alvaro Prieto <source@alvaroprieto.com>
2019-12-26 11:15:45 -0800

    Add /video /videos to timelapse-ui

--- timelapse_ui/src/timelapse_ui.py
+++ timelapse_ui/src/timelapse_ui.py
@@ -2,11 +2,28 @@ import os
 import time
 import tempfile
 import subprocess
+import yaml
 from datetime import datetime, timedelta
 from flask import Flask, request, g, render_template, send_file, redirect
 from v4l2 import v4l2
 
+
 app = Flask(__name__)
+app.config.from_pyfile("default_config")
+app.config.from_envvar("TIMELAPSE_UI_SETTINGS", silent=True)
+
+
+def load_config(config_path):
+    with open(config_path, "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    if config is None:
+        raise Exception("Unable to load config file")
+
+    return config
+
+
+gen_config = load_config(app.config["TIMELAPSE_GEN_CONFIG"])
 
 
 def take_photo(device="/dev/video0", resolution="1920x1080", filename=None):
@@ -91,6 +108,47 @@ def preview_photo():
         filename, mimetype="image/jpeg", attachment_filename="preview.jpg"
     )
     response.headers["Content-Disposition"] = 'inline; filename="preview.jpg"'
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    response.headers["Cache-Control"] = "public, max-age=0"
+
+    return response
+
+
+@app.route("/videos")
+def videos():
+
+    videos = []
+    for config in gen_config["timelapses"]:
+
+        video_path = os.path.join(config["out_dir"], "{}.mp4".format(config["name"]))
+        if os.path.exists(video_path):
+            videos.append({"name": config["name"], "resolution": config["resolution"]})
+
+    return render_template("videos.html", videos=videos)
+
+
+@app.route("/video/<name>")
+def video(name):
+
+    video = None
+    for config in gen_config["timelapses"]:
+        if name != config["name"]:
+            continue
+        video_path = os.path.join(config["out_dir"], "{}.mp4".format(config["name"]))
+        if os.path.exists:
+            video = video_path
+            break
+
+    video_filename = os.path.split(video_path)[1]
+
+    response = send_file(
+        video_path, mimetype="video/mp4", attachment_filename=video_filename
+    )
+    response.headers["Content-Disposition"] = 'inline; filename="{}"'.format(
+        video_filename
+    )
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
