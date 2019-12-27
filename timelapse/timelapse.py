f4d60995e5b277ba069598a9a695ca71f2c4aab4
Alvaro Prieto <source@alvaroprieto.com>
2019-12-27 13:59:12 -0800

    Refactor timelapse.py to be a class instead of a bunch or random functions
    Add timestamp to photos so each timelapse set has a start time
    
    reload config changes and start new timelapse when that happens

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -8,116 +8,123 @@ import yaml
 from datetime import datetime
 
 
-def load_config(config_path):
-    with open(config_path, "r") as config_file:
-        config = yaml.safe_load(config_file)
+class Timelapse:
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self.config = self.load_config(config_path)
 
-    if config is None:
-        raise Exception("Unable to load config file")
+        self.start_timelapse()
 
-    return config
+    def start_timelapse(self):
+        self.last_photo_time = 0
+        self.index = 0
 
-
-def take_photo(device="/dev/video0", resolution="1920x1080", filename=None):
-
-    if filename is None:
-        filename = tempfile.mktemp(".jpg")
-
-    result = subprocess.run(
-        [
-            "fswebcam",
-            "--no-banner",
-            "-sFocus, Auto=False",
-            "-sExposure, Auto=Manual Mode",
-            "-sWhite Balance Temperature, Auto=False",
-            "-S5",
-            "-d",
-            "v4l2:{}".format(device),
-            "-r",
-            resolution,
-            filename,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    # fswebcam seems to use stderr instead of stdout
-    rval = result.stderr.decode("utf-8")
-    if "Error" in rval and "Error querying menu" not in rval:
-        print(rval)
-        return None
-
-    isodate = datetime.now().isoformat()
-
-    result = subprocess.run(
-        [
-            "exiftool",
-            "-overwrite_original",
-            "-DateTimeOriginal='{}'".format(isodate),
-            filename,
-        ],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-
-    return filename
-
-
-last_photo_time = 0
-
-
-def time_for_photo(config):
-    global last_photo_time
-
-    if time.time() >= (last_photo_time + config["interval_s"]):
-        last_photo_time = time.time()
-        return True
-    else:
-        return False
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("config", help="Config file location")
-parser.add_argument("--overwrite", action="store_true", help="Overwrite images if necessary")
-args = parser.parse_args()
-
-if not os.path.exists(args.config):
-    raise Exception("Config file not found.")
-
-config = load_config(args.config)
-
-if not os.path.exists(config["out_dir"]):
-    print("{} not found. Creating".format(config["out_dir"]))
-    os.makedirs(config["out_dir"], exist_ok=True)
-
-index = 0
-
-while True:
-
-    # TODO - setup watchdog to watch config file for changes
-    new_config = load_config(args.config)
-    if new_config != config:
-        print("config changed!")
-    config = new_config
-
-    if time_for_photo(config):
-        filename = os.path.join(
-            config["out_dir"], "{}{:06d}.jpg".format(config["prefix"], index)
+        # Use start time of timelapse as a tag
+        self.photo_prefix = "{}{}_".format(
+            self.config["prefix"], datetime.now().replace(microsecond=0).isoformat()
         )
 
-        if os.path.exists(filename) and args.overwrite:
-            print("Overwriting file {}".format(filename))
-        elif os.path.exists(filename):
-            print("Error: File exists - {}".format(filename))
-            filename = None
+        if not os.path.exists(self.config["out_dir"]):
+            print("{} not found. Creating".format(self.config["out_dir"]))
+            os.makedirs(self.config["out_dir"], exist_ok=True)
 
-        if filename:
-            res = take_photo(config["device"], config["resolution"], filename)
-            if res == filename:
-                print("Saving photo to ", filename)
-            else:
-                print("Error taking photo")
+    def load_config(self, config_path):
+        with open(config_path, "r") as config_file:
+            config = yaml.safe_load(config_file)
 
-        index += 1
+        if config is None:
+            raise Exception("Unable to load config file")
 
-    time.sleep(1)
+        return config
+
+    def time_for_photo(self):
+        if time.time() >= (self.last_photo_time + self.config["interval_s"]):
+            self.last_photo_time = time.time()
+            return True
+        else:
+            return False
+
+    def take_photo(self, device="/dev/video0", resolution="1920x1080", filename=None):
+
+        if filename is None:
+            filename = tempfile.mktemp(".jpg")
+
+        result = subprocess.run(
+            [
+                "fswebcam",
+                "--no-banner",
+                "-sFocus, Auto=False",
+                "-sExposure, Auto=Manual Mode",
+                "-sWhite Balance Temperature, Auto=False",
+                "-S5",
+                "-d",
+                "v4l2:{}".format(device),
+                "-r",
+                resolution,
+                filename,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        # fswebcam seems to use stderr instead of stdout
+        rval = result.stderr.decode("utf-8")
+        if "Error" in rval and "Error querying menu" not in rval:
+            print(rval)
+            return None
+
+        isodate = datetime.now().isoformat()
+
+        result = subprocess.run(
+            [
+                "exiftool",
+                "-overwrite_original",
+                "-DateTimeOriginal='{}'".format(isodate),
+                filename,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        return filename
+
+    def run(self):
+        while True:
+
+            # TODO - setup watchdog to watch config file for changes
+            new_config = self.load_config(args.config)
+            if new_config != self.config:
+                print("Config changed. Starting new timelapse with new settings.")
+                self.start_timelapse()
+            self.config = new_config
+
+            if self.time_for_photo():
+                filename = os.path.join(
+                    self.config["out_dir"],
+                    "{}{:06d}.jpg".format(self.photo_prefix, self.index),
+                )
+
+                res = self.take_photo(
+                    self.config["device"], self.config["resolution"], filename
+                )
+                if res == filename:
+                    print("Saving photo to ", filename)
+                else:
+                    print("Error taking photo")
+
+                self.index += 1
+
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="Config file location")
+    args = parser.parse_args()
+
+    if not os.path.exists(args.config):
+        raise Exception("Config file not found.")
+
+    timelapse = Timelapse(args.config)
+
+    timelapse.run()
